51a473eef3ca5773bacbf5265df12d2db1a8a183
eyad-wd <eyadoffline@gmail.com>
2025-04-21 20:56:52 +0900

    Started working on insights
    Started on Insights and changed some minor things in the JS specifically the fade-in effect, elements don't disappear once they're off screen anymore to ensure stability.
    
    Also  insights is still being worked on just wanted to make a pull request to receive any feedback

---
+++ javascript/app.js
@@ -0,0 +1,63 @@
+// Reveal element effect
+const observer = new IntersectionObserver((entries) => {
+entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show', 'show-2');
+    } 
+});
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// LIVE number
+let liveNumber = 250000000000; // Starting number (250 billion)
+
+function updateLiveNumber() {
+    liveNumber += Math.random() * 5000000; // Increase by a random amount for a realistic effect
+    // Element where the number will be shown.
+    document.getElementById('money').textContent = formatNumber(liveNumber);
+        
+}
+
+function formatNumber(number) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: 0
+    }).format(number);
+}
+
+setInterval(updateLiveNumber, 1000); // Update every second
+
+document.getElementById('money').textContent = formatNumber(liveNumber);
+
+// THIS WHOLE SCRIPT DOESN'T WORK. It'll be worked on later.
+/**/ 
+const swiper = new Swiper('.card-wrapper', {
+loop: true,
+
+// Functionality for the buttons
+navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+},
+
+
+breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+
+    768: {
+        slidesPerView: 2
+    },
+
+    1080: {
+        slidesPerView: 3
+    },
+}
+});
+/* */
